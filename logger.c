46fc986dcf2d99164edee524e58c522da868171a
Matteo <matteomancanelli@gmail.com>
2018-05-08 00:17:32 +0200

    added

--- logger.c
+++ logger.c
@@ -37,16 +37,33 @@ typedef struct DistSensor {
 
 typedef struct WaterSensor {
 	char* desc;
-	float data;
+	float from;
+	float to;
 	uint8_t analog_bit;
 } WaterSensor;
 
 typedef struct TempSensor {
-	char* desc;
+	char* temp_desc;
+	char* hum_desc;
 	float temp_data;
 	float hum_data;
 	uint8_t digital_bit;
 } TempSensor;
+
+typedef enum {
+	GetDist = 0x1,
+	GetWater = 0x2,
+	GetTemp = 0x3,
+	GetAll = 0x4
+} StructType;
+
+typedef enum {
+	DistFloat = 0x1,
+	WaterFrom = 0x2,
+	WaterTo = 0x3,
+	TempFloat = 0x4,
+	HumFloat = 0x5
+} FloatType;
 
 typedef struct LogStruct {
 	DistSensor* ds;
@@ -54,19 +71,35 @@ typedef struct LogStruct {
 	TempSensor* ts;
 	int write_count;
 	int read_count;
+	StructType type;
 } LogStruct;
 
-void struct_init(DistSensor* dist_sensor, WaterSensor* water_sensor, TempSensor* temp_sensor, LogStruct* log_struct) {
-	dist_sensor->desc  = (char*) malloc(32);
-	water_sensor->desc = (char*) malloc(32);
-	temp_sensor->desc  = (char*) malloc(32);
+typedef struct Buffer {
+    void* data;
+    int next;
+    int size;
+} Buffer;
+
+DistSensor*  dist_sensor;
+WaterSensor* water_sensor;
+TempSensor*  temp_sensor;
+LogStruct*   log_struct;
+Buffer* 	 buffer;
+
+void struct_init(DistSensor* dist_sensor, WaterSensor* water_sensor, TempSensor* temp_sensor, LogStruct* log_struct, Buffer* buffer) {
+	dist_sensor->desc  = (char*) malloc(16);
+	water_sensor->desc = (char*) malloc(16);
+	temp_sensor->temp_desc = (char*) malloc(16);
+	temp_sensor->hum_desc  = (char*) malloc(16);
 	
 	dist_sensor->desc = "Distanza";
 	water_sensor->desc = "Profondità";
-	temp_sensor->desc = "Temperatura e Umidità";
-
+	temp_sensor->temp_desc = "Temperatura";
+	temp_sensor->hum_desc = "Umidità";
+	
 	dist_sensor->data = 0.0;
-	water_sensor->data = 0.0;
+	water_sensor->from = 0.0;
+	water_sensor->to = 0.0;
 	temp_sensor->temp_data = 0.0;
 	temp_sensor->hum_data = 0.0;
 	
@@ -78,14 +111,107 @@ void struct_init(DistSensor* dist_sensor, WaterSensor* water_sensor, TempSensor*
 	log_struct->ds = dist_sensor;
 	log_struct->ws = water_sensor;
 	log_struct->ts = temp_sensor;
+	
 	log_struct->read_count = 0;
 	log_struct->write_count = 0;
+	log_struct->type = GetAll;
+	
+	buffer->data = malloc(4);
+	buffer->size = 4;
+    buffer->next = 0;
 }
 
-DistSensor*  dist_sensor;
-WaterSensor* water_sensor;
-TempSensor*  temp_sensor;
-LogStruct*   log_struct;
+void reset_buffer(Buffer* b) {
+	memset(((char*) buffer->data), 0, strlen(buffer->data)+1);
+	buffer->next = 0;
+}
+
+void reserve_space(Buffer* buffer, int bytes) {
+    if((buffer->next + bytes) > buffer->size) {
+        buffer->data = realloc(buffer->data, buffer->size * 2);
+        buffer->size *= 2;
+    }
+}
+
+void serialize_string(char* str, Buffer* b) {
+	int len = strlen(str);
+    reserve_space(b, len+2);
+    memcpy(((char*) b->data) + b->next, str, len);
+    memcpy(((char*) b->data) + b->next + len, ": ", 2);
+    b->next += len+2;
+}
+
+void serialize_float(float x, FloatType float_type, Buffer* b) {
+	char str[8];
+	dtostrf(x, 4, 2, str);
+	
+	int len = strlen(str);
+    reserve_space(b, len+5);
+    memcpy(((char*) b->data) + b->next, str, len);
+    
+    switch(float_type) {
+		case DistFloat:
+		case WaterTo:
+			memcpy(((char*) b->data) + b->next + len, " cm\n", 4);
+			b->next += len+4;
+			break;
+			
+		case WaterFrom:
+			memcpy(((char*) b->data) + b->next + len, "-", 1);
+			b->next += len+1;
+			break;
+		
+		case TempFloat:
+			memcpy(((char*) b->data) + b->next + len, " °C\n", 5);
+			b->next += len+5;
+			break;
+			
+		case HumFloat:
+			memcpy(((char*) b->data) + b->next + len, " %\n", 3);
+			b->next += len+3;
+			break;
+		
+		default:
+			memcpy(((char*) b->data) + b->next + len, "\n", 1);
+			b->next += len+1;
+			break;
+	}
+}
+
+void packet_serialize(LogStruct* ls, Buffer* b) {
+	switch(ls->type) {
+		case GetDist:
+			serialize_string(ls->ds->desc, b);
+			serialize_float(ls->ds->data, DistFloat, b);
+			break;
+		
+		case GetWater:
+			serialize_string(ls->ws->desc, b);
+			serialize_float(ls->ws->from, WaterFrom, b);
+			serialize_float(ls->ws->to, WaterTo, b);
+			break;
+		
+		case GetTemp:
+			serialize_string(ls->ts->temp_desc, b);
+			serialize_float(ls->ts->temp_data, TempFloat, b);
+			serialize_string(ls->ts->hum_desc, b);
+			serialize_float(ls->ts->hum_data, HumFloat, b);
+			break;
+		
+		case GetAll:
+			ls->type = GetDist;
+			packet_serialize(ls, b);
+			ls->type = GetWater;
+			packet_serialize(ls, b);
+			ls->type = GetTemp;
+			packet_serialize(ls, b);
+			break;
+		
+		default:
+			printf("Invalid operation!\n");
+			break;			
+	}
+}
 
 // ==============================================================
 // ========================== UART ==============================
@@ -96,7 +222,7 @@ void UART_init() {
 	UBRR0L = BAUD_PRESCALE;
 	
 	UCSR0A |= bit_value(U2X0);
-	//UCSR0B |= bit_value(RXCIE0);
+	UCSR0B |= bit_value(RXCIE0);
 	UCSR0B |= bit_value(RXEN0)  | bit_value(TXEN0);
 	UCSR0C |= bit_value(UCSZ00) | bit_value(UCSZ01) | bit_value(USBS0);
 }
@@ -158,7 +284,7 @@ void EEPROM_erase(int n) {
 		eeprom_write_byte((uint8_t*) i, 0);
 }
 
-int EEPROM_init() {
+void EEPROM_init() {
 	int data;
 	cli();
 	
@@ -174,7 +300,6 @@ int EEPROM_init() {
 	
 	if(eeprom_empty)
 		printf("EEPROM empty\n");
-	return eeprom_empty;
 }
 
 // ==============================================================
@@ -200,31 +325,22 @@ unsigned long pulseIn(uint8_t state, unsigned long timeout) {
 	
 	unsigned long width = 0;
 	unsigned long numloops = 0;
-	
-	// convert the timeout from microseconds to a number of times through
-	// the initial loop; it takes 16 clock cycles per iteration
 	unsigned long maxloops = microsecondsToClockCycles(timeout) / 16;
 
-	// wait for any previous pulse to end
 	while ((PINB & bit) == stateMask)
 		if (numloops++ == maxloops)
 			return 0;
 	
-	// wait for the pulse to start
 	while ((PINB & bit) != stateMask)
 		if (numloops++ == maxloops)
 			return 0;
 
-	// wait for the pulse to stop
 	while ((PINB & bit) == stateMask) {
 		if (numloops++ == maxloops)
 			return 0;
 		width++;
 	}
 
-	// convert the reading to microseconds. The loop has been determined
-	// to be 20 clock cycles long and have about 16 clocks between the edge
-	// and the start of the loop
 	return clockCyclesToMicroseconds(width * 16 + 16);
 }
 
@@ -233,25 +349,25 @@ void dist_printf(float f_val) {
 	printf("Distanza: %d.%d cm\n", i_val / 10, i_val % 10);
 }
 
-void dist_sensor_active(char* str) {
+void dist_sensor_active() {
 	char res[16];
-	float max = 0.0;
-	
+
 	begin_trig();
 	
 	unsigned long duration = pulseIn(HIGH, 10000);
 	float cm = (duration/2) / 29.1;
 	
-	max = (cm > max) ? cm : max;
+	//dist_printf(cm);
+	dist_sensor->data = cm;
 	
-	dist_printf(cm);
 	_delay_ms(500);
 	
-	dtostrf(max, 4, 2, res);
-	//sprintf(res, "%u", (uint32_t) max);
+	/*
+	dtostrf(cm, 4, 2, res);
 	strcpy(str, "Distanza: ");
 	strcat(str, res);
 	strcat(str, " cm");
+	*/
 }
 
 // ==============================================================
@@ -287,31 +403,43 @@ uint16_t analog_read() {
 	return (((uint16_t) hb) << 8) | lb;
 }
 
-void analog_sensor_active(char* str) {
+void analog_sensor_active() {
 	uint16_t val = analog_read();
 	if(0 <= val && val < 130) {
-		strcpy(str, "Profondità: 0.0-0.5 cm");
-		printf("%s\n", str);
+		//strcpy(str, "Profondità: 0.0-0.5 cm");
+		//printf("%s\n", str);
+		water_sensor->from = 0.0;
+		water_sensor->to = 0.5;
 	}
 	if(130 <= val && val < 260)	{
-		strcpy(str, "Profondità: 0.5-1.0 cm");
-		printf("%s\n", str);
+		//strcpy(str, "Profondità: 0.5-1.0 cm");
+		//printf("%s\n", str);
+		water_sensor->from = 0.5;
+		water_sensor->to = 1.0;
 	}
 	if(260 <= val && val < 320)	{
-		strcpy(str, "Profondità: 1.0-2.0 cm");
-		printf("%s\n", str);
+		//strcpy(str, "Profondità: 1.0-2.0 cm");
+		//printf("%s\n", str);
+		water_sensor->from = 1.0;
+		water_sensor->to = 2.0;
 	}
 	if(320 <= val && val < 390)	{
-		strcpy(str, "Profondità: 2.0-3.0 cm");
-		printf("%s\n", str);
+		//strcpy(str, "Profondità: 2.0-3.0 cm");
+		//printf("%s\n", str);
+		water_sensor->from = 2.0;
+		water_sensor->to = 3.0;
 	}
 	if(390 <= val && val < 460)	{
-		strcpy(str, "Profondità: 3.0-4.0 cm");
-		printf("%s\n", str);
+		//strcpy(str, "Profondità: 3.0-4.0 cm");
+		//printf("%s\n", str);
+		water_sensor->from = 3.0;
+		water_sensor->to = 4.0;
 	}
 	if(val >= 460)	{
-		strcpy(str, "Profondità: >4.0 cm");
-		printf("%s\n", str);
+		//strcpy(str, "Profondità: >4.0 cm");
+		//printf("%s\n", str);
+		water_sensor->from = 4.0;
+		water_sensor->to = 5.0;
 	}
 	_delay_ms(500);
 }
@@ -355,37 +483,36 @@ uint8_t temp_data() {
 	return c;
 }
 
-void temp_sensor_active(char* str) {
+void temp_sensor_active() {
 	char res1[16];
 	char res2[16];
 	
-	float hum, temp, checksum;
-	float max_t, max_h;
-	
+	float hum = 52.71, temp = 25.64, checksum;
+
 	temp_request();
 	temp_response();
-	
+
 	hum = ((float) temp_data()) + ((float) (temp_data() / 100));
 	temp = ((float) temp_data()) + ((float) (temp_data() / 100));
 	checksum = (float) temp_data();
 	
-	printf("T: %u °C\n", (uint32_t) temp);
-	printf("H: %u %%\n", (uint32_t) hum);
+	temp_sensor->temp_data = temp;
+	temp_sensor->hum_data = hum;
 	
-	max_t = (temp > max_t) ? temp : max_t;
-	max_h = (hum  > max_h) ? hum  : max_h;
+//	printf("T: %u °C\n", (uint32_t) temp);
+//	printf("H: %u %%\n", (uint32_t) hum);
 	
 	_delay_ms(500);
 	
-	dtostrf(max_t, 4, 2, res1);
-	dtostrf(max_h, 4, 2, res2);
-	//sprintf(res1, "%u", (uint32_t) temp);
-	//sprintf(res2, "%u", (uint32_t) hum);
+	/*
+	dtostrf(temp, 4, 2, res1);
+	dtostrf(hum, 4, 2, res2);
 	strcpy(str, "Temp: ");
 	strcat(str, res1);
 	strcat(str, " °C; Um: ");
 	strcat(str, res2);
 	strcat(str, " %");
+	*/
 }
 
 // ==============================================================
@@ -397,87 +524,107 @@ void sensor_init() {
 	analog_sensor_init();
 }
 
+ISR(USART_RX_vect) {
+	printf("Mode [p, w, r, e]: ");
+	char mode = UART_receive(&INFP);
+	printf("%c\n", mode);
+	
+	cli();
+	reset_buffer(buffer);
+	
+	int data;
+	EEPROM_read_int(&data, 0);
+	int eeprom_empty = !data;
+	
+	char str[256];
+	int len;
+			
+	switch(mode) {
+		case 'P':
+		case 'p':
+			log_struct->type = GetAll;
+			packet_serialize(log_struct, buffer);
+			
+			printf("%s", (char*) buffer->data);
+			break;
+		
+		case 'W':
+		case 'w':
+			printf("Store results in EEPROM\n");
+			
+			log_struct->write_count++;
+			printf("write_count: %d\n", log_struct->write_count);
+			
+			log_struct->type = GetAll;
+			packet_serialize(log_struct, buffer);
+			
+			len = strlen(buffer->data)+1;
+			EEPROM_write_int(len, 0);
+			EEPROM_write(buffer->data, 1, len);
+			
+			eeprom_empty = 0;
+			printf("EEPROM written\n");
+			break;
+
+		case 'R':
+		case 'r':
+			if(eeprom_empty) {
+				printf("EEPROM empty!\n");
+				break;
+			}
+			
+			log_struct->read_count++;
+			printf("read_count: %d\n", log_struct->read_count);
+			
+			EEPROM_read_int(&len, 0);
+			EEPROM_read(str, 1, len);
+			
+			printf("Contents of string in EEPROM:\n");
+			printf("%s", str);
+			break;
+		
+		case 'E':
+		case 'e':
+			EEPROM_erase(1000);
+			eeprom_empty = 1;
+			
+			log_struct->read_count = 0;
+			log_struct->write_count = 0;
+			
+			printf("EEPROM erased\n");
+			break;
+		
+		default:
+			printf("Invalid operation!\n");
+			break;
+	}
+	
+	printf("----------------------------\n");
+	sei();
+}
+
 int main() {
 	dist_sensor  = (DistSensor*)  malloc(sizeof(DistSensor));
 	water_sensor = (WaterSensor*) malloc(sizeof(WaterSensor));
 	temp_sensor  = (TempSensor*)  malloc(sizeof(TempSensor));
 	log_struct   = (LogStruct*)   malloc(sizeof(LogStruct));
-	
-	char res1[32], res2[32], res3[32];
-	int len1, len2, len3;
+	buffer		 = (Buffer*)	  malloc(sizeof(Buffer));
 	
 	UART_init();
 	stdout = &OUTFP;
 	stdin  = &INFP;
 	
-	int eeprom_empty = EEPROM_init();
-	struct_init(dist_sensor, water_sensor, temp_sensor, log_struct);
+	EEPROM_init();
 	sensor_init();
 	
+	struct_init(dist_sensor, water_sensor, temp_sensor, log_struct, buffer);
+	
+	sei();
+	
 	while(1) {
-		printf("Mode [w, r, e]: ");
-		char mode = UART_receive(&INFP);
-		printf("%c\n", mode);
-		
-		switch(mode) {
-			case 'W':
-			case 'w':
-				printf("Store results in EEPROM\n");
-				
-				dist_sensor_active(res1);
-				analog_sensor_active(res2);
-				temp_sensor_active(res3);
-
-				len1 = strlen(res1) + 1;
-				len2 = strlen(res2) + 1;
-				len3 = strlen(res3) + 1;
-								
-				EEPROM_write_int(len1, 0);
-				EEPROM_write(res1, 1, len1);
-				
-				EEPROM_write_int(len2, len1+1);
-				EEPROM_write(res2, len1+2, len2);
-				
-				EEPROM_write_int(len3, len1+len2+2);
-				EEPROM_write(res3, len1+len2+3, len3);
-				
-				eeprom_empty = 0;
-				printf("EEPROM written\n");
-				break;
-
-			case 'R':
-			case 'r':
-				if(eeprom_empty) {
-					printf("EEPROM empty!\n");
-					break;
-				}
-				
-				EEPROM_read_int(&len1, 0);
-				EEPROM_read(res1, 1, len1);
-				
-				EEPROM_read_int(&len2, len1+1);
-				EEPROM_read(res2, len1+2, len2);
-				
-				EEPROM_read_int(&len3, len1+len2+2);
-				EEPROM_read(res3, len1+len2+3, len3);
-								
-				printf("Contents of string in EEPROM:\n");
-				printf("\t %s\n", res1);
-				printf("\t %s\n", res2);
-				printf("\t %s\n", res3);
-				break;
-			
-			case 'E':
-			case 'e':
-				EEPROM_erase(1000);
-				eeprom_empty = 1;
-				
-				printf("EEPROM erased\n");
-				break;
-			
-			default:
-				printf("Invalid operation!\n");
-				break;
-		}
+		dist_sensor_active();
+		analog_sensor_active();
+		temp_sensor_active();
+		_delay_ms(1000);
 	}
 }
